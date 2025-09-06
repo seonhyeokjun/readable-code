f3f1344b4f205536d493ed40ae1fc36067c1e47f
seonhyeokjun <tjsrmatn@naver.com>
2025-09-07 03:19:04 +0900

    DIP: Dependency Inversion Principle

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -2,12 +2,16 @@ package cleancode.minesweeper.tobe;
 
 import cleancode.minesweeper.tobe.gamelevel.Advanced;
 import cleancode.minesweeper.tobe.gamelevel.GameLevel;
+import cleancode.minesweeper.tobe.io.ConsoleInputHandler;
+import cleancode.minesweeper.tobe.io.ConsoleOutputHandler;
 
 public class GameApplication {
     public static void main(String[] args) {
         GameLevel gameLevel = new Advanced();
+        ConsoleInputHandler consoleInputHandler = new ConsoleInputHandler();
+        ConsoleOutputHandler consoleOutputHandler = new ConsoleOutputHandler();
 
-        Minesweeper minesweeper = new Minesweeper(gameLevel);
+        Minesweeper minesweeper = new Minesweeper(gameLevel, consoleInputHandler, consoleOutputHandler);
         minesweeper.initialize();
         minesweeper.run();
     }
